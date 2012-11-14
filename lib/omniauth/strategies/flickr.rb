9fd17ac3f351f0b3361d0de28034e052f3388692
kineticac <kineticac@gmail.com>
2012-11-14 15:30:00 -0800

    logging to debug

--- lib/omniauth/strategies/flickr.rb
+++ lib/omniauth/strategies/flickr.rb
@@ -69,6 +69,8 @@ module OmniAuth
 
       def request_phase
         options[:authorize_params] = {:perms => options[:scope]} if options[:scope]
+        puts options.to_yaml
+        puts client.to_yaml if client
         super
       end
     end
